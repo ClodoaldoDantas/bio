e0cc272b8d5d791603a1bf8aec488244af43b24c
Clodoaldo Dantas <clodoaldodantas8@gmail.com>
2024-04-20 09:30:18 -0300

    feat: add new icons and update link component

--- src/data/links.ts
+++ src/data/links.ts
@@ -1,17 +1,21 @@
 export const links = [
   {
+    icon: "/icons/github.svg",
     label: "Github",
     url: "https://github.com/ClodoaldoDantas",
   },
   {
+    icon: "/icons/linkedin.svg",
     label: "Linkedin",
     url: "https://www.linkedin.com/in/clodoaldodantas/",
   },
   {
+    icon: "/icons/instagram.svg",
     label: "Instagram",
     url: "https://www.instagram.com/clodoaldodantas8/",
   },
   {
+    icon: "/icons/coffee.svg",
     label: "Portfólio",
     url: "https://www.clodoaldodantas.com.br/",
   },
